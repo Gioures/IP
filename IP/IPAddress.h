358e5d8713f8d35b757f4ae92e2c5e31a889d99e
Gioures <756119874@qq.com>
2020-09-21 09:49:20 +0800

    Add files via upload

---
+++ IP/IPAddress.h
@@ -0,0 +1,16 @@
+//
+//  IPAddress.h
+//  Ailiao
+//
+//  Created by apple on 11-10-31.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IPAddress : NSObject
+{
+    
+}
+- (NSString *) GetLocalIPAddress;
+@end
